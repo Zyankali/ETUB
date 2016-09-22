dd700a06a1062a4e326cf91668d1b6bdaa1fc760
Zyankali <silentsands@web.de>
2016-09-22 20:33:01 +0200

    Lovely äöüs
    I HATE EM I HATE HTML.

--- installer/install_etub.php
+++ installer/install_etub.php
@@ -215,7 +215,7 @@ echo "Ende der Protokolldatei <br>";
 ?>
 
 <p align="left">Sollten keine Fehler auf zu finden sein. So klicken Sie bitte auf den Verweis/Link &quot;Abschluss&quot;.<br>
-Ansonsten wiederholen Sie die Installationsroutine und beachten Sie ihre einträge und die hinweise. <br>
+Ansonsten wiederholen Sie die Installationsroutine und beachten Sie ihre eintr&auml;ge und die hinweise. <br>
 Dazu rufen sie einfach den Link &quot;Wiederholen&quot; auf.</p>
 
 <br>
