f8a7a9e306f79f15c24d4775d0f292374f79970e
Zyankali <silentsands@web.de>
2016-11-29 22:14:55 +0100

    Designelements
    Added some Icons and changed a bit the css for the table borders. Also
    Implimented the clock and date icons into the main side.

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ echo "<h1>EasyToUseBlog</h1>";
 </ul>
 
 <div>
-<?php 
+<?php
 
 if (file_exists("admin/connect.php")) {
 require_once ('admin/connect.php');
@@ -43,28 +43,28 @@ echo "Fehler: Konnte keine Verbindung zur Datenbank herstellen.";
 /* Seitenermittelung der anzuzeigenden Einträge. Hierbei sind es Pro Seite 20 einträge.*/
 
 if (!isset($_GET['seite']) or empty ($_GET['seite']))
-	
-	{
-		
-		$_GET['seite'] = 1;
-		
-	}
+
+        {
+
+                $_GET['seite'] = 1;
+
+        }
 
 if (!isset($seite))
-	
-	{
-		
-		$seite = 1;
-		
-	}	
-	
-	
+
+        {
+
+                $seite = 1;
+
+        }
+
+
 if ($resultat = mysqli_query($db_link, "SELECT inhalt FROM kontent ORDER BY id")) {
-	
+
     // Bestimme wie viele eintraege in der Datenbank "kontent" vorhanden ist
     $row_cnt = mysqli_num_rows($resultat);
-	
-	$a = $row_cnt;
+
+        $a = $row_cnt;
     // Schließe und gebe Variable $resultat frei
     mysqli_free_result($resultat);
 }
@@ -74,9 +74,9 @@ $seite = $_GET['seite'];
 if (!is_numeric ($seite))
 
 {
-	
-	die ("Fehler! Bitte Laden Sie die Seite neu."); 
-	
+
+        die ("Fehler! Bitte Laden Sie die Seite neu.");
+
 }
 
 // Bestimmen wie viele Einträge auf einmal angezeigt werden sollen.
@@ -85,68 +85,68 @@ $seitenanzahl = $a / 20;
 
 
 // Wenn mehr übermittelt wurde als errechnet oder möglich ist oder keine Zahl ermittelt wurde, die "seite" Variable resetten auf eins! Sollte vor den meisten beabischtigten und unbeabsichtigten crashes in dem Fall schützen
-	if (is_int($seite))
-		{
-			
-			$seite = 1;
-			
-		}
+        if (is_int($seite))
+                {
+
+                        $seite = 1;
+
+                }
 
 if ($seite >= $seitenanzahl + 1)
-	{
-	
-		$seite = 1; 
-		
-		
-	}
-	
+        {
+
+                $seite = 1;
+
+
+        }
+
 
 // Wenn nur eine Seite ermittelt wurde dann zeige auch nur eine Seite an
 if ($seitenanzahl == "1")
 {
-	
-	echo '<a href="index.php">1</a>'; 
-	
+
+        echo '<a href="index.php">1</a>';
+
 }
 
 // Wenn mehere Seiten ermittelt wurden dann Zeige Links zu den anderen Seiten an und ermittle den eigenen Seitenzählstand.
 if ($seitenanzahl > "1")
-	
-	{
-		
-	$seitenunten = $seite; 
-		
-	$counta = 1;
-	while($counta < $seite)
-		{
-		echo '<a href="index.php?seite=';
-		echo $counta;
-		echo '"> ';
-		echo $counta;
-		echo ' |</a>';
-		$counta++;
-		}
-	
-	echo " ";
-	echo $seite;
-	echo " ";
 
-	$seitenoben = $seite + 1;
-		
-	while ($seitenoben < $seitenanzahl + 1)
-		
-		{
-		echo '<a href="index.php?seite=';
-		echo $seitenoben;
-		echo '">| ';
-		echo $seitenoben;
-		echo ' </a>';
-		$seitenoben++;
-		}
+        {
+
+        $seitenunten = $seite;
+
+        $counta = 1;
+        while($counta < $seite)
+                {
+                echo '<a href="index.php?seite=';
+                echo $counta;
+                echo '"> ';
+                echo $counta;
+                echo ' |</a>';
+                $counta++;
+                }
+
+        echo " ";
+        echo $seite;
+        echo " ";
+
+        $seitenoben = $seite + 1;
+
+        while ($seitenoben < $seitenanzahl + 1)
+
+                {
+                echo '<a href="index.php?seite=';
+                echo $seitenoben;
+                echo '">| ';
+                echo $seitenoben;
+                echo ' </a>';
+                $seitenoben++;
+                }
 
 }
 
-	
+
 
 if ($seite != "1")
 {
@@ -154,12 +154,12 @@ $eintragsoffset = $seite * 20 - 20;
 }
 else
 {
-	
-	$eintragsoffset = 0;
-	
+
+        $eintragsoffset = 0;
+
 }
 
-/* Ende der Seiten und Eintragsermittelung*/ 
+/* Ende der Seiten und Eintragsermittelung*/
 $sql = "SELECT id, titel, inhalt, authoren, zeit, datum FROM kontent ORDER BY id DESC LIMIT 20 OFFSET $eintragsoffset";
 
 // den query an mysql übertragen und anschließend abfragen.
@@ -193,10 +193,10 @@ $inhalt = preg_replace($suchmusterd, $ersetzungd, $inhalt);
 $suchmustere = '/\[youtube\]+\b(http|https):\/\/.*\/(watch\?v=|embed\/|)([\w]+)\b\[\/youtube\]/i';
 $ersetzunge = '<iframe width="560" height="315" src="https://www.youtube.com/embed/$3" frameborder="0" allowfullscreen></iframe>';
 $inhalt = preg_replace($suchmustere, $ersetzunge, $inhalt);
-	
+
 echo "<table>";
 echo "<tr>";
-echo "<th id=\"smalltext\" colspan=\"2\">Author: " . $row->authoren . " | " . $row->zeit . " | " . $row->datum . "</th>";
+echo "<th id=\"smalltext\" colspan=\"2\">Author: " . $row->authoren . " | <img src=\"icons/clok.png\" alt=\"time\" border=\"0\" width=\"14\" height=\"14\"> " . $row->zeit . " | <img src=\"icons/date.png\" alt=\"date\" border=\"0\" width=\"14\" height=\"14\"> " . $row->datum . "</th>";
 echo "</tr>";
 echo "<tr>";
 echo "<th class=\"titel\" colspan=\"2\"><idler>" . $row->id . " |</idler> <titel>" . $row->titel . "</titel></td>";
@@ -206,7 +206,7 @@ echo "<th class=\"inhalt\" colspan=\"2\">" . $inhalt . "</th>";
 
 echo "</tr>";
 echo "</table>";
- 
+
 }
 
 ?>
